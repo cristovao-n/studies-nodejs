90ee7bef741d5af0f4f6401fc77d37fa8cc1363c
cristovao-n <cristovaon13@gmail.com>
2023-07-07 02:45:41 -0300

    reset migrations

--- nodejs/prisma/prisma/migrations/20230707054518_init/migration.sql
+++ nodejs/prisma/prisma/migrations/20230707054518_init/migration.sql
@@ -1,17 +1,8 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "SubmissionLanguage" AS ENUM ('JAVA', 'PYTHON');
 
 -- CreateEnum
-CREATE TYPE "SubmissionStatus" AS ENUM ('ACCEPTED', 'COMPILATION_ERROR', 'RUNTIME_ERROR', 'WRONG_ANSWER', 'PRESENTATION_ERROR', 'TIMEOUT');
-
--- DropTable
-DROP TABLE "User";
+CREATE TYPE "SubmissionStatus" AS ENUM ('PENDING', 'ACCEPTED', 'COMPILATION_ERROR', 'RUNTIME_ERROR', 'WRONG_ANSWER', 'PRESENTATION_ERROR', 'TIMEOUT');
 
 -- CreateTable
 CREATE TABLE "Admin" (
@@ -19,6 +10,8 @@ CREATE TABLE "Admin" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
 );
@@ -29,6 +22,8 @@ CREATE TABLE "Participant" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Participant_pkey" PRIMARY KEY ("id")
 );
@@ -36,34 +31,28 @@ CREATE TABLE "Participant" (
 -- CreateTable
 CREATE TABLE "Contest" (
     "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
+    "acronym" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
     "cover" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "place" TEXT NOT NULL,
     "capacity" INTEGER NOT NULL,
     "startDate" TIMESTAMP(3) NOT NULL,
     "endDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Contest_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "ContestAdmin" (
+CREATE TABLE "ParticipantsJoinContests" (
     "id" SERIAL NOT NULL,
     "contestId" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
-
-    CONSTRAINT "ContestAdmin_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "ContestParticipant" (
-    "id" SERIAL NOT NULL,
-    "contestId" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "participantId" INTEGER NOT NULL,
     "levelId" INTEGER NOT NULL,
 
-    CONSTRAINT "ContestParticipant_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ParticipantsJoinContests_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -71,6 +60,8 @@ CREATE TABLE "Rule" (
     "id" SERIAL NOT NULL,
     "contestId" INTEGER NOT NULL,
     "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Rule_pkey" PRIMARY KEY ("id")
 );
@@ -85,6 +76,8 @@ CREATE TABLE "Question" (
     "outputExplanation" TEXT NOT NULL,
     "testsFileInput" TEXT NOT NULL,
     "testsFileOutput" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
 );
@@ -94,6 +87,8 @@ CREATE TABLE "Constraint" (
     "id" SERIAL NOT NULL,
     "questionId" INTEGER NOT NULL,
     "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Constraint_pkey" PRIMARY KEY ("id")
 );
@@ -104,6 +99,8 @@ CREATE TABLE "InputOutputExample" (
     "questionId" INTEGER NOT NULL,
     "input" TEXT NOT NULL,
     "output" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "InputOutputExample_pkey" PRIMARY KEY ("id")
 );
@@ -112,18 +109,12 @@ CREATE TABLE "InputOutputExample" (
 CREATE TABLE "Level" (
     "id" SERIAL NOT NULL,
     "contestId" INTEGER NOT NULL,
-    "name" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Level_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "QuestionLevel" (
-    "id" SERIAL NOT NULL,
-    "questionId" INTEGER NOT NULL,
-    "levelId" INTEGER NOT NULL,
-
-    CONSTRAINT "QuestionLevel_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -134,19 +125,15 @@ CREATE TABLE "Submission" (
     "levelId" INTEGER NOT NULL,
     "status" "SubmissionStatus" NOT NULL,
     "code" TEXT NOT NULL,
-    "language" TEXT NOT NULL,
+    "language" "SubmissionLanguage" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "_AdminToContest" (
-    "A" INTEGER NOT NULL,
-    "B" INTEGER NOT NULL
-);
-
--- CreateTable
-CREATE TABLE "_ContestToParticipant" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
 );
@@ -164,16 +151,19 @@ CREATE UNIQUE INDEX "_AdminToContest_AB_unique" ON "_AdminToContest"("A", "B");
 CREATE INDEX "_AdminToContest_B_index" ON "_AdminToContest"("B");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_ContestToParticipant_AB_unique" ON "_ContestToParticipant"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_ContestToParticipant_B_index" ON "_ContestToParticipant"("B");
-
--- CreateIndex
 CREATE UNIQUE INDEX "_LevelToQuestion_AB_unique" ON "_LevelToQuestion"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_LevelToQuestion_B_index" ON "_LevelToQuestion"("B");
+
+-- AddForeignKey
+ALTER TABLE "ParticipantsJoinContests" ADD CONSTRAINT "ParticipantsJoinContests_contestId_fkey" FOREIGN KEY ("contestId") REFERENCES "Contest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ParticipantsJoinContests" ADD CONSTRAINT "ParticipantsJoinContests_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ParticipantsJoinContests" ADD CONSTRAINT "ParticipantsJoinContests_levelId_fkey" FOREIGN KEY ("levelId") REFERENCES "Level"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Rule" ADD CONSTRAINT "Rule_contestId_fkey" FOREIGN KEY ("contestId") REFERENCES "Contest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -204,12 +194,6 @@ ALTER TABLE "_AdminToContest" ADD CONSTRAINT "_AdminToContest_A_fkey" FOREIGN KE
 
 -- AddForeignKey
 ALTER TABLE "_AdminToContest" ADD CONSTRAINT "_AdminToContest_B_fkey" FOREIGN KEY ("B") REFERENCES "Contest"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_ContestToParticipant" ADD CONSTRAINT "_ContestToParticipant_A_fkey" FOREIGN KEY ("A") REFERENCES "Contest"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_ContestToParticipant" ADD CONSTRAINT "_ContestToParticipant_B_fkey" FOREIGN KEY ("B") REFERENCES "Participant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_LevelToQuestion" ADD CONSTRAINT "_LevelToQuestion_A_fkey" FOREIGN KEY ("A") REFERENCES "Level"("id") ON DELETE CASCADE ON UPDATE CASCADE;
