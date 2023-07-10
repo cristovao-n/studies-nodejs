de8d3d3e939731b0d9e59b42580f2715530075d7
cristovao-n <cristovaon13@gmail.com>
2023-07-10 02:11:34 -0300

    fix database seed feature

--- nodejs/prisma/prisma/seed.ts
+++ nodejs/prisma/prisma/seed.ts
@@ -3,17 +3,6 @@ import { prisma } from "../src/lib/prisma";
 const CONTEST_DESCRIPTION = `A Olimpíada Paraibana de Informática visa despertar nos alunos o interesse em computação imprescindível na formação básica dos estudantes atualmente, através de uma atividade que envolve desafios motivadores e competição saudável. Esta competição também permite que os competidores possam conhecer de forma mais abrangente a carreira na área. Já para os alunos do ensino superior, a OPI visa estimular os alunos a aprimorarem e aprofundarem o conhecimento em Programação e Algoritmos. Além disso, a olimpíada almeja preparar melhor e despertar o interesse de alunos paraibanos para as competições nacionais e internacionais de programação, como a Olimpíada Brasileira e Internacional de Informática, e a ICPC. Estes alunos possivelmente se motivarão a entrar no curso de Ciência da Computação, e o conhecimento adquirido na preparação será muito valioso para a sua formação. A organização da OPI está a cargo do curso de Ciência da Computação da Universidade Federal de Campina Grande.`;
 
 async function main() {
-  await prisma.admin.deleteMany();
-  await prisma.participant.deleteMany();
-  await prisma.contest.deleteMany();
-  await prisma.rule.deleteMany();
-  await prisma.level.deleteMany();
-  await prisma.participantsJoinContests.deleteMany();
-  await prisma.question.deleteMany();
-  await prisma.constraint.deleteMany();
-  await prisma.inputOutputExample.deleteMany();
-  await prisma.submission.deleteMany();
-
   await prisma.admin.create({
     data: {
       name: "Cristovao",
